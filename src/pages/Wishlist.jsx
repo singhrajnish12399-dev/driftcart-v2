0b6ed9e605bdb51b65cb193348982d52a625678d
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-11 09:54:00 +0530

    Update Wishlist.jsx

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,7 +1,20 @@
-export default function Wishlist() {
+function Wishlist({ wishlist }) {
   return (
-    <div>
-      <h2>❤️ Wishlist Page</h2>
+    <div style={{ padding: "15px" }}>
+      <h1>❤️ Wishlist</h1>
+
+      {wishlist.length === 0 ? (
+        <p>No Wishlist Items</p>
+      ) : (
+        wishlist.map((item) => (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+            <p>₹{item.price}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
+
+export default Wishlist;
