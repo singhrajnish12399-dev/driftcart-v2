505797f03ebe7e85c944a97842aa46ca6bed24fb
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-11 10:36:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Wishlist from "./pages/Wishlist";
-import Product from "./pages/Product";
-
 function App() {
-  const [cart, setCart] = useState([]);
-  const [wishlist, setWishlist] = useState([]);
-
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Home
-            cart={cart}
-            wishlist={wishlist}
-          />
-        }
-      />
-
-      <Route
-        path="/cart"
-        element={<Cart cart={cart} />}
-      />
-
-      <Route
-        path="/wishlist"
-        element={<Wishlist wishlist={wishlist} />}
-      />
-
-      <Route
-        path="/product/:id"
-        element={<Product />}
-      />
-    </Routes>
+    <div>
+      <h1>DriftCart Working ✅</h1>
+    </div>
   );
 }
 
