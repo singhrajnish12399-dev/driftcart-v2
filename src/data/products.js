502a5cc9c3f7b258001d26b92e7d29f92df1ad37
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-11 09:52:00 +0530

    Create src/data/products.js

---
+++ src/data/products.js
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 1,
+    name: "Premium T-Shirt",
+    category: "Fashion",
+    price: 499,
+    image:
+      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
+  },
+  {
+    id: 2,
+    name: "Running Shoes",
+    category: "Fashion",
+    price: 1499,
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+  },
+  {
+    id: 3,
+    name: "Smart Watch",
+    category: "Electronics",
+    price: 999,
+    image:
+      "https://images.unsplash.com/photo-1523170335258-f5ed11844a49"
+  },
+  {
+    id: 4,
+    name: "Headphones",
+    category: "Electronics",
+    price: 1999,
+    image:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+  }
+];
