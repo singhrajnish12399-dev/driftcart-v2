c4c590dbf9a1a66ad6d359a23909944fb8695017
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-11 09:54:23 +0530

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,32 @@
-export default function Product() {
+import { useParams } from "react-router-dom";
+import { products } from "../data/products";
+
+function Product() {
+  const { id } = useParams();
+
+  const product = products.find(
+    (p) => p.id === Number(id)
+  );
+
+  if (!product) {
+    return <h2>Product Not Found</h2>;
+  }
+
   return (
-    <div>
-      <h2>📦 Product Page</h2>
+    <div style={{ padding: "15px" }}>
+      <img
+        src={product.image}
+        alt={product.name}
+        width="100%"
+      />
+
+      <h2>{product.name}</h2>
+
+      <h3>₹{product.price}</h3>
+
+      <p>{product.category}</p>
     </div>
   );
 }
+
+export default Product;
