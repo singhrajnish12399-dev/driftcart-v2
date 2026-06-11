d8b01e56564e5f12baee795522f886b7b148b5ca
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-11 09:53:41 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,20 @@
-export default function Cart() {
+function Cart({ cart }) {
   return (
-    <div>
-      <h2>🛒 Cart Page</h2>
+    <div style={{ padding: "15px" }}>
+      <h1>🛒 Cart</h1>
+
+      {cart.length === 0 ? (
+        <p>Cart Empty</p>
+      ) : (
+        cart.map((item) => (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+            <p>₹{item.price}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
+
+export default Cart;
